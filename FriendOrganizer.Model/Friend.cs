acafbf07ddda0d51d23df542d39420873e0900ce
Maria Alexandra Boros <boros.maria.alexandra@hotmail.com>
2024-01-21 19:30:42 +0200

    Add project files.

---
+++ FriendOrganizer.Model/Friend.cs
@@ -0,0 +1,13 @@
+﻿namespace FriendOrganizer.Model
+{
+    public class Friend
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public int LastName { get; set; }
+
+        public int Email { get; set; }
+    }
+}
